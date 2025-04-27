3f5f8a14982c374cc7dbd672d9cd194f31dbb531
vivek <vivek@gmail.com>
2025-04-28 01:54:22 +0530

    final

--- controllers/uploadCVController.js
+++ controllers/uploadCVController.js
@@ -92,6 +92,9 @@ const sendCVEmail = async (name, email, phone, subject, message, cv) => {
     
     const transporter = nodemailer.createTransport({
       service: "gmail",
+      // host: process.env.EMAIL_HOST,  
+      // port: process.env.EMAIL_PORT,  
+      // secure: process.env.EMAIL_SECURE === "true", 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
